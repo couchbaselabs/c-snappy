7614229eb9dd210bfe4d83dc9d4643f4ad37e04d
Marty Schoch <marty.schoch@gmail.com>
2016-02-12 15:23:29 -0500

    first round of fixes for windows

--- import.sh
+++ import.sh
@@ -6,9 +6,9 @@ rm -rf *.cc internal/*
 curl -sL https://github.com/google/snappy/archive/1.1.3.tar.gz | tar zxf - -C internal --strip-components=1
 (cd internal && ./autogen.sh && ./configure)
 
-# symlink so cgo compiles them
+# copy (not symlink thanks to windows) so cgo compiles them
 for source_file in $(make sources); do
-  ln -sf $source_file .
+  cp $source_file .
 done
 
 # restore the repo to what it would look like when first cloned.
